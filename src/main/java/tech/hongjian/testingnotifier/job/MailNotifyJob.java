5c5badb3dadce9b0faf263511892387ca963c872
xiahongjian <hongjian.xia@qq.com>
2021-04-26 21:50:15 +0800

    :ok_hand: 修改MailNotifyJob，可以通过参数指定邮件标题和内容

--- src/main/java/tech/hongjian/testingnotifier/job/MailNotifyJob.java
+++ src/main/java/tech/hongjian/testingnotifier/job/MailNotifyJob.java
@@ -2,6 +2,7 @@ package tech.hongjian.testingnotifier.job;
 
 import lombok.Setter;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -29,34 +30,40 @@ public class MailNotifyJob implements Job {
     @SneakyThrows
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        String htmlContent = "<h1>标题</h1>\n" +
-                "\n" +
-                "<table>\n" +
-                "    <tr>\n" +
-                "        <th>第一列</th>\n" +
-                "        <th>第二列</th>\n" +
-                "        <th>第二列</th>\n" +
-                "    " +
-                "</tr>\n" +
-                "    <tr>\n" +
-                "        <td>1</td>\n" +
-                "        <td><a href='https://baidu.com' target='_blank'>https://baidu.com</a></td>\n" +
-                "        <td>百度</td>\n" +
-                "    " +
-                "</tr>\n" +
-                "    <tr>\n" +
-                "        <td>2</td>\n" +
-                "        <td><a href='https://baidu.com' target='_blank'>https://baidu.com</a></td>\n" +
-                "        <td>百度</td>\n" +
-                "    </tr>\n" +
-                "    <tr>\n" +
-                "        <td>3</td>\n" +
-                "        <td><a href='https://baidu.com' target='_blank'>https://baidu.com</a></td>\n" +
-                "        <td>百度</td>\n" +
-                "    </tr>\n" +
-                "</table>";
-
         String to = ScheduleUtil.getJobStringParam(context, "to");
-        notificationService.sendMail(new String[]{to}, "测试邮件", htmlContent);
+        String htmlContent = ScheduleUtil.getJobStringParam(context, "content");
+        if (StringUtils.isBlank(htmlContent)) {
+            htmlContent = "<h1>标题</h1>\n" +
+                    "\n" +
+                    "<table>\n" +
+                    "    <tr>\n" +
+                    "        <th>第一列</th>\n" +
+                    "        <th>第二列</th>\n" +
+                    "        <th>第二列</th>\n" +
+                    "    " +
+                    "</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>1</td>\n" +
+                    "        <td><a href='https://baidu.com' target='_blank'>https://baidu.com</a></td>\n" +
+                    "        <td>百度</td>\n" +
+                    "    " +
+                    "</tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>2</td>\n" +
+                    "        <td><a href='https://baidu.com' target='_blank'>https://baidu.com</a></td>\n" +
+                    "        <td>百度</td>\n" +
+                    "    </tr>\n" +
+                    "    <tr>\n" +
+                    "        <td>3</td>\n" +
+                    "        <td><a href='https://baidu.com' target='_blank'>https://baidu.com</a></td>\n" +
+                    "        <td>百度</td>\n" +
+                    "    </tr>\n" +
+                    "</table>";
+        }
+        String subject = ScheduleUtil.getJobStringParam(context, "subject");
+        if (StringUtils.isBlank(subject)) {
+            subject = "测试邮件";
+        }
+        notificationService.sendMail(new String[]{to}, subject, htmlContent);
     }
 }
